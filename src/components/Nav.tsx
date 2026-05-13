e5e92d612e1c087df8c0645272b544b2f3c4acc1
IVK <kravchenkort@gmail.com>
2026-05-13 11:34:56 +0300

    Fixed commiting issue

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 // src/components/Nav.tsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 
 const Nav: React.FC = () => {
