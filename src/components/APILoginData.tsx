337597a51e2468297514919a99bed4e518b35d25
TabL0ck <miscritscrits@gmail.com>
2026-05-15 14:07:07 +0300

    Fixed name of pogo pin model

--- src/components/APILoginData.tsx
+++ src/components/APILoginData.tsx
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
-  SERVICE_ID: '',      // ← замени на свой
-  TEMPLATE_ID: '',    // ← замени на свой
-  PUBLIC_KEY: '',      // ← замени на свой
+  SERVICE_ID: 'service_tob41qn',      // ← замени на свой
+  TEMPLATE_ID: 'template_emrt4gy',    // ← замени на свой
+  PUBLIC_KEY: 'rPjKJgzDMGnnI8pCb',      // ← замени на свой
 };
