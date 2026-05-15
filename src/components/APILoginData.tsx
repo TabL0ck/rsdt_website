028f149869c8f83854e83fa31bd8e8410142583d
TabL0ck <miscritscrits@gmail.com>
2026-05-15 14:16:56 +0300

    Removed API keys

--- src/components/APILoginData.tsx
+++
@@ -1,5 +0,0 @@
-export const EMAILJS_CONFIG = {
-  SERVICE_ID: 'service_tob41qn',      // ← замени на свой
-  TEMPLATE_ID: 'template_emrt4gy',    // ← замени на свой
-  PUBLIC_KEY: 'rPjKJgzDMGnnI8pCb',      // ← замени на свой
-};
